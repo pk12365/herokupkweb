c2f635311735122ec9eee54919f2a300bf52b781
pardeepsingh12365 <pardeepsingh12365@gmail.com>
2017-12-17 22:14:39 +0530

    Update set.py

--- cogs/set.py
+++ cogs/set.py
@@ -11,15 +11,15 @@ class set:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(no_pm=True, pass_context=True, name="givealt",
-                      aliases=["helpalt", "althelp"])
+    @commands.command(pass_context=True, name="getalt",
+                      aliases=["helpalt", "althelp", "getalt", "Getalt", "GETALT", "Helpalt", "HelpAlt", "HELPALT", "altHelp", "altHELP", "ALTHELP"])
     @checks.is_main_server()
-    async def givealt(self):
+    async def getalt(self):
 
         #Your code will go here
-        await self.bot.say('```we are giving some alt only on\nINDIAN CYBER WORLD\nJust take a command $get(your command)\n\nAccount List\nminecraft\nSpotify\nNetflix\nHulu\nOrigin\nUplay```')
+        await self.bot.say('```we are giving some 🆓alt only on\n💟INDIAN CYBER WORLD💟\nJust take a command $get(your command)\n\nAccount List⤵\n\n🔴minecraft\n🔵Spotify\n⚪Netflix\n⚫Hulu\n🔴Origin\n🔵Uplay```')
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="getminecraft", aliases=["getMinecraft", "getMINECRAFT"])
     @checks.is_main_server()
     async def getminecraft(self):
         """done"""
@@ -28,7 +28,7 @@ class set:
         await self.bot.say("check your dm bebe 😅😅", delete_after=5.0)
         await self.bot.whisper(random.choice(minecraft.minecraft_accounts))
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="getspotify", aliases=["getSpotify", "getSPOTIFY", "GETSPOTIFY"])
     @checks.is_main_server()
     async def getspotify(self):
         """done"""
@@ -37,7 +37,7 @@ class set:
         await self.bot.say("check your dm bebe 😅😅", delete_after=5.0)
         await self.bot.whisper(random.choice(spotify.spotify_accounts))
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="getnetflix", aliases=["getNetflix", "getNETFLIX", "GETNETFLIX"])
     @checks.is_main_server()
     async def getnetflix(self):
         """done"""
@@ -46,7 +46,7 @@ class set:
         await self.bot.say("check your dm bebe 😅😅", delete_after=5.0)
         await self.bot.whisper(random.choice(netflix.netflix_accounts))
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="hulu", aliases=["getHulu", "getHULU", "GETHULU"])
     @checks.is_main_server()
     async def gethulu(self):
         """done"""
@@ -55,7 +55,7 @@ class set:
         await self.bot.say("check your dm bebe 😅😅", delete_after=5.0)
         await self.bot.whisper(random.choice(hulu.hulu_accounts))
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="origin", aliases=["getOrigin", "getORIGIN", "GETORIGIN"])
     @checks.is_main_server()
     async def getorigin(self):
         """done"""
@@ -64,7 +64,7 @@ class set:
         await self.bot.say("check your dm bebe 😅😅", delete_after=5.0)
         await self.bot.whisper(random.choice(origin.origin_accounts))
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, name="getuplay", aliases=["getUplay", "getUPLAY", "GETUPLAY"])
     @checks.is_main_server()
     async def getuplay(self):
         """done"""
