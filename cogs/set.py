d4687d3bd9a74da73436174e08865be07d349a9e
pardeepsingh12365 <pardeepsingh12365@gmail.com>
2017-12-19 13:00:29 +0530

    Update set.py

--- cogs/set.py
+++ cogs/set.py
@@ -17,9 +17,8 @@ class get:
         field_name = "Generic Name"
         field_contents = "Example contents for this field"
         footer_text = ""
-        embed_colours = ("0×FF0000", "0×FF69B4", "0×FFFF00")
 
-        embed = discord.Embed(colour=0×FF69B4, description=line1)
+        embed = discord.Embed(colour=0×FF0000, description=line1)
         embed.title = "INDIAN CYBER WORLD"
         embed.set_footer(text=footer_text)
         await self.bot.say(embed=embed)
